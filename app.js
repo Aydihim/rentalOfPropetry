df7979e7a8384bad893f0d1172e47c1905cf5414
Aydima <basyada_holonda@mail.ru>
2023-03-08 12:13:13 +0300

    config

---
+++ app.js
@@ -0,0 +1,20 @@
+require('@babel/register');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const renderComponents = require('./middleware/renderComponent');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(renderComponents);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(morgan('dev'));
+
+app.listen(PORT, () => {
+  console.log(`Работаем на ${PORT} порту`);
+});
