1fd275d1c3fef8d2fa9a7ce5779c6124ddae330f
Egor <goncharenko.egor174@gmail.com>
2023-03-08 14:17:48 +0300

    routes1

---
+++ routes/house.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+const CategoryList = require('../components/categoryList');
+
+router.get('/', (req, res) => {
+  try {
+    res.renderComponent(CategoryList, { title: 'Houses' });
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
+});
+
+module.exports = router;
