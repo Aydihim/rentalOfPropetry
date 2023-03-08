b88ddb2068da095a305b486273ad77adbd003d9f
Egor <goncharenko.egor174@gmail.com>
2023-03-08 15:41:41 +0300

    logo

--- PropertyParams.jsx
+++ PropertyParams.jsx
@@ -6,7 +6,7 @@ const PropertyParams = ({ property, title }) => {
     <Layout title={title}>
       <div className="card" style={{ width: '18rem' }}>
         <p>полная инфа</p>
-        <img src={property.img} className="card-img-top" alt="..." />
+        <img src={property.img} className="card-img-top" alt="logo" />
         <div className="card-body">
           <h5 className="card-title">{property.title}</h5>
           <p>{property.price}</p>
